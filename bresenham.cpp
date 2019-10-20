9f7aeff99fbe629537f56b902b84edd79f304e53
npdr <p.nieuwenhoff@usp.br>
2019-10-20 16:02:59 -0300

    now bresenham is finished xd

--- bresenham.cpp
+++ bresenham.cpp
@@ -1,11 +1,31 @@
-
 #include <stdio.h>
+#include <math.h>
 
 void bresenham(int x1, int y1, int x2, int y2) {
     int dx, dy, p, x, y;
 
+    /* Vertical line */
+    if(x1 == x2) {
+        x = x1;
+        y = y1;
+
+        if(y2 >= y1) {
+            while(y <= y2) {
+                printf("(%d,%d)\n", x, y);
+                y++;
+            }
+        }
+
+        if(y2 < y1)
+        while(y >= y2) {
+            printf("(%d,%d)\n", x, y);
+            y--;
+        }
+        return;
+    }
+
     /* Slope less than or equal to 1 */
-    if(( (y2 - y1) / (x2 - x1) ) <= 1) {
+    if(fabs( (y2 - y1) / (x2 - x1) ) <= 1) {
         dx = x2 - x1;
         dy = y2 - y1;
 
@@ -24,6 +44,7 @@ void bresenham(int x1, int y1, int x2, int y2) {
                     y++;
                 }
             }
+            return;
         }
 
         if (x2 >= x1 && y2 <= y1) {
@@ -38,6 +59,7 @@ void bresenham(int x1, int y1, int x2, int y2) {
                     y--;
                 }
             }
+            return;
         }
 
         if (x2 <= x1 && y2 >= y1) {
@@ -52,6 +74,7 @@ void bresenham(int x1, int y1, int x2, int y2) {
                     y++;
                 }
             }
+            return;
         }
 
         if (x2 <= x1 && y2 <= y1) {
@@ -66,11 +89,13 @@ void bresenham(int x1, int y1, int x2, int y2) {
                     y--;
                 }
             }
+            return;
         }
+        return;
     }
 
     /* Slope bigger than 1 */
-    if(( (y2 - y1) / (x2 - x1) ) > 1) {
+    if(fabs( (y2 - y1) / (x2 - x1) ) > 1) {
         int aux = x1;
         x1 = y1;
         y1 = aux;
@@ -143,8 +168,12 @@ void bresenham(int x1, int y1, int x2, int y2) {
 }
 
 int main() {
-    int x1 = 1, y1 = 1, x2 = 3, y2 = 6;
-    
+    //int x1 = 1, y1 = 1, x2 = 3, y2 = 6;
+    //int x1 = 3, y1 = 6, x2 = 1, y2 = 1;
+    //int x1 = 3, y1 = 1, x2 = 1, y2 = 6;
+    //int x1 = 1, y1 = 6, x2 = 3, y2 = 1;
+    //int x1 = 8, y1 = 3, x2 = 2, y2 = 3;  
+    int x1 = 1, y1 = 6, x2 = 1, y2 = 1;
     bresenham(x1, y1, x2, y2);
     return 0;
 }
